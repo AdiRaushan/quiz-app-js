fa9c573016200b83e48f88d304d2c45a31c79bd8
AdiRaushan <adiraushan2k@gmail.com>
2023-05-30 17:51:04 +0530

    master

---
+++ app.js
@@ -0,0 +1,52 @@
+const quizData = [
+    {
+        question: "how old is Adi",
+        a: "12",
+        b: "17",
+        c: "56",
+        d: "20",
+        correct: "d"
+    },{
+        question: "what is the best programming language?",
+        a: "python",
+        b: "c++",
+        c: "javascript",
+        d: "rust",
+        correct: "a"
+    },{
+        question: "which one these is not type of loop?",
+        a: "for loop",
+        b: "while loop",
+        c: "map function",
+        d: "string",
+        correct: "d"
+    },{
+        question: "Which of the following is not a data type?",
+        a: "integer",
+        b: "string",
+        c: "loop",
+        d: "boolean",
+        correct: "c"
+    },{
+        question: "What is the purpose of a loop in programming ?",
+        a: "To store data",
+        b: "To make decisions",
+        c: "to repeat a block of code",
+        d: "to performe mathmatical operation",
+        correct: "c"
+    },{
+        question: "Which of the following is an example of an integrated development environment (IDE)?",
+        a: "microsoft word",
+        b: "google crome",
+        c: "pycharm",
+        d: "photoshop",
+        correct: "c"
+    }, {
+        question: "Which of the following is a programming language?",
+        a: "python",
+        b: "HTML",
+        c: "CSS",
+        d: "Java",
+        correct: "b"
+    }
+]
